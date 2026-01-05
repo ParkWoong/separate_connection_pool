bb2c2aee982a200b6d2fa3213fec09c7c2719e50
ParkWoong <ww714@naver.com>
2026-01-06 00:27:16 +1000

    comment : comment for test

--- connection_pool/src/main/java/com/example/connection_pool/service/TestService.java
+++ connection_pool/src/main/java/com/example/connection_pool/service/TestService.java
@@ -1,10 +1,6 @@
 package com.example.connection_pool.service;
 
 import javax.sql.DataSource;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.example.connection_pool.dto.DataRow;
@@ -14,29 +10,29 @@ import com.zaxxer.hikari.HikariDataSource;
 
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TestService {
-    private static final Logger log = LoggerFactory.getLogger(TestService.class);
 
     private final DataMapper dataMapper;
     private final LogMapper appLogMapper;
-
     
-    @Resource(name = "mainDataSource")
-    private final DataSource mainDataSource;
+    @Resource(name = "mainDataSource") 
+    private final DataSource mainDataSource; // 로그를 위한 필드주입 (실제 서비스에서는 사용 x)
 
     @Resource(name = "logDataSource")
     private final DataSource logDataSource;
 
     public DataRow fetchData(long id) {
-        log.info("[POOL] data SELECT will use: {}", poolName(mainDataSource));
+        log.info("[POOL] data SELECT use: {}", poolName(mainDataSource));
         return dataMapper.selectById(id);
     }
 
     public int writeLog(String message) {
-        log.info("[POOL] log INSERT will use: {}", poolName(logDataSource));
+        log.info("[POOL] log INSERT use: {}", poolName(logDataSource));
         return appLogMapper.insertLog(message);
     }
 
